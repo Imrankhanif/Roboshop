56ee1ee43d00ea62eaa74a916499cebe80add1f5
imran <imrankhanmohammed130@gmail.com>
2025-03-06 21:03:51 -0600

    Installing frontend App

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,19 @@
-dfdfdfdsfd
 
-jhgjhfhdgfhgjhgjhkj
+echo -e "\e[33m Installing Nginx server \e[0m"
+yum install nginx -y 
+echo -e "\e[33m Removing old app content \e[0m"
+
+rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[33m Downloading Froentend content \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
+echo -e "\e[35m extract Froentend content \e[0m"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf 
+
+systemctl enable nginx 
+systemctl restart nginx 
