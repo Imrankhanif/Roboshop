13cb3e7c59eabd6eafc49cb29508492e30245c92
imran <imrankhanmohammed130@gmail.com>
2025-03-06 21:56:14 -0600

    sjds

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,22 @@
 
 echo -e "\e[33m Installing Nginx server\e[0m"
-dnf install nginx -y 
+dnf install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[34m enable Nginx server\e[0m"
-systemctl enable nginx 
+systemctl enable nginx &>>/tmp/roboshop.log
 echo -e "\e[33m start Nginx server\e[0m"
-systemctl start nginx 
+systemctl start nginx &>>/tmp/roboshop.log
 echo -e "\e[33m remove old app content\e[0m"
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
 echo -e "\e[33m extract frontend contend\e[0m"
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html  &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip   &>>/tmp/roboshop.log
 
 
 #vim /etc/nginx/default.d/roboshop.conf 
 
 echo -e "\e[33m restart frontend\e[0m"
-systemctl restart nginx 
+systemctl restart nginx  &>>/tmp/roboshop.log
