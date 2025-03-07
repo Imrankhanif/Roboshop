576d25d92b2153e31c599baee958e31712b14e93
imran <imrankhanmohammed130@gmail.com>
2025-03-06 22:04:50 -0600

    mongodb

--- frontend.sh
+++ frontend.sh
@@ -9,14 +9,14 @@ echo -e "\e[33m remove old app content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
 
 echo -e "\e[33m extract frontend contend\e[0m"
 cd /usr/share/nginx/html  &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip   &>>/tmp/roboshop.log
 
 
-#vim /etc/nginx/default.d/roboshop.conf 
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
 echo -e "\e[33m restart frontend\e[0m"
 systemctl restart nginx  &>>/tmp/roboshop.log
---
+++ mongodb.sh
@@ -0,0 +1,8 @@
+dnf install mongodb-org -y 
+
+
+systemctl enable mongod 
+systemctl start mongod 
+
+
+systemctl restart mongod
