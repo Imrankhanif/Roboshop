137ab7e494f3e49361d2a35634b1641000f430db
imran <imrankhanmohammed130@gmail.com>
2025-03-09 16:11:35 -0500

    added redirect

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
 echo -e "\e[33m Copy mongodb repo file\e[0m"
-cp mongo.repo /home/centos/Roboshop/mongod.conf
+cp mongo.repo /home/centos/Roboshop/mongod.conf   &>>/tmp/roboshop.log
 
 echo -e "\e[33m Install Mongodb\e[0m"
-dnf install mongodb-org -y 
+dnf install mongodb-org -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33m enable mondodb\e[0m"
-systemctl enable mongod 
+systemctl enable mongod  &>>/tmp/roboshop.log
 echo -e "\e[33m start mongodb\e[0m"
-systemctl start mongod 
+systemctl start mongod  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Restart mongodb\e[0m"
-systemctl restart mongod
+systemctl restart mongod &>>/tmp/roboshop.log
