4462f46d5e1177fd11b0c35116fb54583d4eeb93
imran <imrankhanmohammed130@gmail.com>
2025-03-09 16:24:33 -0500

    asd

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,9 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo   &>>/tmp/roboshop.log
 echo -e "\e[33m Install Mongodb\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 
+ sed -i 's/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+
+
 echo -e "\e[33m enable mondodb\e[0m"
 systemctl enable mongod  &>>/tmp/roboshop.log
 echo -e "\e[33m start mongodb\e[0m"
